2dc7b05081fbcdbd8e1f135488e3f9859df645fb
doumova <doumogueye.mg@gmail.com>
2021-05-09 02:54:23 +0000

    send mail with mailtrp

---
+++ app/Mail/SignupEmail.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class SignupEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('doumogueye.mg@gmail.com')->subject('Welcome to Globaltechnologie')->view('mail.signup-email')->with('data',$this->data);
+    }
+}
---
+++ resources/views/mail/signup-email.blade.php
@@ -0,0 +1,14 @@
+Hello
+<br><br>
+Welcome to may web site!
+
+<br>
+Plaise click the below link to verify your email and activate your account!
+<br><br>
+
+<a href="#">Click here</a>
+
+<br><br>
+Think you
+<br>
+Global tech
